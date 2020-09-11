61c6cbc3422a19b1f7266a173ec605dfafe67722
Nicolas Texidor <NICOLAS.TEXIDOR@correo.ucu.edu.uy>
2020-09-11 19:33:41 -0300

    test 2

--- PII_Debug_And_Test_Person-master/test/Library.Test/PersonTests.cs
+++ PII_Debug_And_Test_Person-master/test/Library.Test/PersonTests.cs
@@ -17,9 +17,50 @@ namespace Tests
 
 }
         [Test]
-        public void Test1() // Cambiá el nombre para indicar qué estás probando
+        public void TestPerson() // Cambiá el nombre para indicar qué estás probando
         {
-            // Insertá tu código  de pruebaaquí
+        public class Person
+    {
+        public Person(string name, string id)
+        {
+            this.Name = name;
+            this.ID = id;
+        }
+
+        private string name;
+
+        private string id;
+
+        public string Name
+        {
+            get
+            {
+                return this.name;
+            }
+            
+            set
+            {
+                if (!string.IsNullOrEmpty(value))
+                {
+                    this.name = value;
+                }
+            }
+        }
+        public string ID
+        {
+            get
+            {
+                return this.id;
+            }
+
+            set
+            {
+                if (IdUtils.IdIsValid(value))
+                {
+                    this.id = value;
+                }
+            }
         }
     }
-}
+        }
+    }
